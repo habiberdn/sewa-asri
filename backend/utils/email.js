22ab435f25a745b2856424e316d7f5469026c5b8
Habib <habib.erdian@gmail.com>
2023-12-27 18:07:03 +0700

    update trasport email

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,6 +1,9 @@
 const nodemailer = require('nodemailer');
 const { convert } = require('html-to-text');
 const pug = require('pug')
+const Transport = require("nodemailer-brevo-transport");
+
+
 
 module.exports = class Email {
   constructor(user, url) {
