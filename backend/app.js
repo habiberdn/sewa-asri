57de499967819315e909ef3d58025c81a8a961ac
Habib <habib.erdian@gmail.com>
2024-01-18 17:33:02 +0700

    handle cors origin

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,11 @@ const cookieParser = require('cookie-parser');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '10kb' }));
-app.use(cors())
+app.use(cors( {
+    origin: ['http://localhost:5173'],
+    credentials: true,            //access-control-allow-credentials:true
+    methods: ["POST", "GET"],
+  }))
 app.use(cookieParser());
 const limiter = rateLimit({
     max: 100,
