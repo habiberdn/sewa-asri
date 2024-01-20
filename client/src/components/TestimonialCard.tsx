fc5b20323fdeed4500bf15d5614a8dbda904d5d9
Dev Julian <dev.achmadjulian@gmail.com>
2024-01-21 06:48:19 +0700

    refactor: create TestimonialCardProps to make component reusable

--- client/src/components/TestimonialCard.tsx
+++ client/src/components/TestimonialCard.tsx
@@ -1,23 +1,31 @@
-export function TestimonialCard() {
+interface TestimonialCardProps {
+    name: string;
+    photo: string;
+    role: string;
+    reviews: string;
+}
+
+export function TestimonialCard({ name, photo, role, reviews }: TestimonialCardProps) {
     return (
         <article className="testimonial-card">
             <p className="p-regular reviews">
-                Sewa Asri Tenant dinilai sangat mudah digunakan, baik oleh pengguna pemula maupun pengguna yang sudah berpengalaman. Fitur-fiturnya dirancang dengan intuitif dan mudah dipahami.
+                { reviews }
             </p>
 
             <section className="detail">
                 <div aria-label="circle" className="circle" />
 
-                <img    src="https://i.pinimg.com/564x/b7/ee/1e/b7ee1ed702d5c2f24663e767accafd02.jpg"
-                        className="photo" />
+                <img    src={photo}
+                        className="photo" 
+                        loading="lazy" />
 
                 <article className="name-role-wrapper">
                     <h4 className="h4-regular">
-                        Reny Anggraini
+                        { name }
                     </h4>
 
                     <p className="label-regular">
-                        Pengelola villa
+                        { role }
                     </p>
                 </article>
             </section>
