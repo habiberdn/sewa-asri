5d0c6c66b04d126ae335a72ef322bdbaaf1241a4
Dev Julian <dev.achmadjulian@gmail.com>
2024-01-24 18:14:11 +0700

    build: build client with vite

--- client/src/hooks/useVilla.ts
+++ client/src/hooks/useVilla.ts
@@ -1,44 +1,50 @@
 import useSWRMutation from "swr/mutation";
 import { CreateVillaInterface } from "../utils/villa-interfaces";
+import { getUser } from '../utils/userStore';
 
 const API = "https://wild-lime-newt-wig.cyclic.app/api/v1";
 
 interface Argument {
-    userId: string;
-    token: string;
-    villa: CreateVillaInterface;
+    villa: CreateVillaInterface | null;
 }
 
 function useCreateVilla() {
     async function fetcher(url:string, { arg }: { arg: Argument }) {
-        return await fetch(`${API}${url}/${arg.userId}`, {
+        console.info("Fetcher: ", arg.villa);
+        return await fetch(`${API}${url}/6595478b6f9fc196058aced5`, {
             method: 'POST',
 
             headers: {
                 "Content-Type": "application/json",
-                "Cookie": arg.token
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1OTU0NzhiNmY5ZmMxOTYwNThhY2VkNSIsImlhdCI6MTcwNTk3NjM4NiwiZXhwIjoxNzA1OTk3OTg2fQ.4lzOduNo3gj6bQYIMJhHKUS8bbAnCyseGB_nNNnu5Mw`
             },
 
             body: JSON.stringify({
-                ...arg
+                ...arg.villa
             })
         }).then(res => res.json())
     }
 
-    const { data, error, trigger, isMutating } = useSWRMutation("/villa", fetcher);
+    const { data, error, trigger: createNewVilla, isMutating: isCreating } = useSWRMutation("/villa", fetcher);
     
-    return { data, error, trigger, isMutating };
+    return { data, error, createNewVilla, isCreating };
 }
 
 function useUpdateVilla() {
+    const user = getUser();
+
     async function fetcher(url:string, { arg }: { arg: Argument }) {
-        return await fetch(`${API}${url}/${arg.userId}`, {
+        return await fetch(`${API}${url}/${user?.id}`, {
             method: 'PUT',
 
             headers: {
                 "Content-Type": "application/json",
                 "Cookie": "token"
-            }
+            },
+
+            body: JSON.stringify({
+                arg
+            })
         }).then(res => res.json())
     }
 
