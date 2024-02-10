afe105c4d78beadd52b9af4e467eb4901ba29268
Dev Julian <dev.achmadjulian@gmail.com>
2024-02-10 17:38:06 +0700

    fix: add production url to fix CORS

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const cookieParser = require('cookie-parser');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '10kb' }));
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://sewa-asri.netlify.app'],
     credentials: true,            //access-control-allow-credentials:true
     methods: ["POST", "GET"],
 }))
