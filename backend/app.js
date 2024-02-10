26ed1f2e34b2ca8e743903e0fa6d7d2eebe63679
Habib <habib.erdian@gmail.com>
2024-02-10 17:55:09 +0700

    fix: fixing cors on production

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use(function(req, res, next) {
     )
     res.header('Access-Control-Allow-Origin', 'sewa-asri.netlify.app'); // Replace with your client's origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+    res.header('Access-Control-Allow-Headers', "Content-Type, X-Requested-With, Authorization");
     next()
   })
 
