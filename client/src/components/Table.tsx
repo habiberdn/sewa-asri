dc9866672c95c8b21553083869c4a4a74186cc6e
Dev Julian <dev.achmadjulian@gmail.com>
2024-02-14 06:35:04 +0700

    feat: add Table component

---
+++ client/src/components/Table.tsx
@@ -0,0 +1,61 @@
+import "./../styles/css/components/table.css";
+
+interface ReservationSchedule {
+    id: string;
+    
+    date: string;
+    status: string;
+    total: number;
+
+    guest: {
+        id: string;
+        name: string;
+        photo: string;
+    };
+
+    villa: {
+        id: string;
+        name: string;
+    };
+ 
+    schedule: {
+        checkIn: string;
+        checkOut: string;
+    }
+}
+
+export function ReservationTable({ data }: { data: Array<ReservationSchedule> }) {
+    return (
+        <table className="table">
+            <thead className="header">
+                <tr className="row">
+                    <th scope="col" className="cell cell-small label-medium">No</th>
+                    <th scope="col" className="cell cell-small label-medium">Date</th>
+                    <th scope="col" className="cell cell-large label-medium">Guest name</th>
+                    <th scope="col" className="cell cell-large label-medium">Villa name</th>
+                    <th scope="col" className="cell cell-large label-medium">Status</th>
+                    <th scope="col" className="cell cell-large label-medium">Total</th>
+                    <th scope="col" className="cell cell-medium label-medium">Check in</th>
+                    <th scope="col" className="cell cell-medium label-medium">Check out</th>
+                </tr>
+            </thead>
+
+            <tbody className="body">
+                {data.map(reservation => {
+                    return (
+                        <tr className="row">
+                            <td className="cell cell-small label-regular">No</td>
+                            <td className="cell cell-small label-regular">{ reservation.date }</td>
+                            <td className="cell cell-large label-regular">{ reservation.guest.name }</td>
+                            <td className="cell cell-large label-regular">{ reservation.villa.name }</td>
+                            <td className="cell cell-large label-regular">{ reservation.status }</td>
+                            <td className="cell cell-large label-regular">Rp. { reservation.total }</td>
+                            <td className="cell cell-medium label-regular">{ reservation.schedule.checkIn }</td>
+                            <td className="cell cell-medium label-regular">{ reservation.schedule.checkOut }</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
